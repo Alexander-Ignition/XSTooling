a3eead94adda5598af0855299445e1d9612f4afe
Alexander Ignatiev <izh.sever@gmail.com>
2022-11-19 13:26:24 +0300

    Create codecov.sh

---
+++ .github/scripts/codecov.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Usage:
+#    ./.github/scripts/codecov.sh $(swift test --show-codecov-path)
+
+set -eu
+
+# /Users/<user-name>/<package-name>/.build/<platform>/debug/codecov/<package-name>.json
+CODECOV_PATH=$1
+CODECOV_DIR=$(dirname "$CODECOV_PATH")
+PACKAGE_NAME=$(basename "$CODECOV_PATH" .json)
+
+# /Users/<user-name>/<package-name>/.build/<platform>/debug/codecov/default.profdata
+PROFDATA_PATH="$CODECOV_DIR/default.profdata"
+
+# /Users/<user-name>/<package-name>/.build/<platform>/debug/XSToolingPackageTests.xctest/Contents/MacOS/XSToolingPackageTests
+EXCUTABLE_PATH="$(dirname "$CODECOV_DIR")/${PACKAGE_NAME}PackageTests.xctest/Contents/MacOS/${PACKAGE_NAME}PackageTests"
+
+echo "Package name: $PACKAGE_NAME"
+echo "Profdata path: $PROFDATA_PATH"
+echo "Executable path: $EXCUTABLE_PATH"
+
+xcrun llvm-cov report \
+    "$EXCUTABLE_PATH" \
+    -instr-profile="$PROFDATA_PATH" \
+    -ignore-filename-regex=".build|Tests"
