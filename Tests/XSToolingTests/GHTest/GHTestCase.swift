ba21f33f5119ddef7bba0db7df6706fb84da115c
Alexander Ignatiev <izh.sever@gmail.com>
2022-12-12 18:49:32 +0300

    Update GHTestCase.swift

--- Tests/XSToolingTests/GHTest/GHTestCase.swift
+++ Tests/XSToolingTests/GHTest/GHTestCase.swift
@@ -21,23 +21,10 @@ class GHTestCase: XCTestCase {
 #if os(Linux)
 
     override func recordFailure(withDescription description: String, inFile filePath: String, atLine lineNumber: Int, expected: Bool) {
-        if !expected, gitHub.isEnabled {
+        if gitHub.isEnabled {
             gitHub.error(file: filePath, line: lineNumber, message: description)
         }
         super.recordFailure(withDescription: description, inFile: filePath, atLine: lineNumber, expected: expected)
-    }
-
-#else // os(Darwin)
-
-    override func record(_ issue: XCTIssue) {
-        if gitHub.isEnabled {
-            if let location = issue.sourceCodeContext.location {
-                gitHub.error(file: location.fileURL.absoluteString, line: location.lineNumber, message: issue.compactDescription)
-            } else {
-                gitHub.error(message: issue.compactDescription)
-            }
-        }
-        super.record(issue)
     }
 
 #endif
